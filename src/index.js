11980a118b4b13985c7214a44b2a2edf532cf01d
Aditya Agarwal <adityaa803@gmail.com>
2020-02-02 10:42:52 +0530

    render App component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('hello world');
+// src/index.js
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return <h1>Aditya Agarwal</h1>;
+}
+
+const elemRoot = document.getElementById('root');
+
+ReactDOM.render(<App />, elemRoot);
