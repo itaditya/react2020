6859653f5d6e9c3c305b4fd82b0b2d68c4bb9059
Aditya Agarwal <adityaa803@gmail.com>
2020-02-02 11:56:42 +0530

    move App.js into separate file

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// src/App.js
+
+import React from 'react';
+
+function AddToCartButton() {
+  return <button>Add To Cart</button>
+}
+
+function App() {
+  return (
+    <AddToCartButton />
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -3,24 +3,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function AddToCartButton() {
-  return (
-    <button>
-      Add To Cart
-    </button>
-  );
-}
-
-const productDescription = <p>This product will change your life</p>;
-
-function App() {
-  return (
-    <>
-      {productDescription}
-      <AddToCartButton />
-    </>
-  );
-}
+import App from './App.js';
 
 const elemRoot = document.getElementById('root');
 
