4efe8959ef1278a920c6cb76970e041aa3afc157
Aditya Agarwal <adityaa803@gmail.com>
2020-02-02 11:27:41 +0530

    render component inside components

--- src/index.js
+++ src/index.js
@@ -3,8 +3,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function AddToCartButton() {
+  return (
+    <button>
+      Add To Cart
+    </button>
+  );
+}
+
+const productDescription = <p>This product will change your life</p>;
+
 function App() {
-  return <h1>Aditya Agarwal</h1>;
+  return (
+    <>
+      {productDescription}
+      <AddToCartButton />
+    </>
+  );
 }
 
 const elemRoot = document.getElementById('root');
