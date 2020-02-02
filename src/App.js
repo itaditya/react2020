8a930c2448a1b3a91344ff4f5c7b2f562911cf98
Aditya Agarwal <adityaa803@gmail.com>
2020-02-02 17:13:37 +0530

    basic carousel

--- src/App.js
+++ src/App.js
@@ -2,30 +2,32 @@
 
 import React from 'react';
 
-import BuyButton from './BuyButton.js';
+import './style.css';
 
-const products = [{ id: 'abc', name: 'T-Shirt' }, { id: 'xyz', name: 'Jeans' }];
+const images = ['MTkwODU2NA', 'v104en-YP', '7Q76qhEAF', 'jLtSJss72', 'Z6mrcccZv'];
+
+function Carousel() {
+  return (
+    <ul className="image-list">
+      {
+        images.map(image => {
+          const url = `https://cdn2.thecatapi.com/images/${image}.jpg`;
+          return (
+            <li key={image} className="image-item">
+              <img src={url} alt="Cat picture" width="800" height="600" />
+            </li>
+          );
+        })
+      }
+    </ul>
+  );
+}
 
 function App() {
   return (
-    <ul>
-      {
-        products.map(product => (
-          <li key={product.id}>
-            <p>{product.name}</p>
-            <BuyButton
-              id="custom-button"
-              className="class-not-applied"
-              onBuy={() => alert(product.name)}
-              data-action="Buy"
-              onClick={() => console.log('I am never called')}
-            >
-              Buy {product.name}
-            </BuyButton>
-          </li>
-        ))
-      }
-    </ul>
+    <div>
+      <Carousel />
+    </div>
   );
 }
 
